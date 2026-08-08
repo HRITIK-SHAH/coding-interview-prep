42c2c22de2ea989939e6ffb70e50bf3ea19a5f80
Hritik shah <130695406+HRITIK-SHAH@users.noreply.github.com>
2026-08-08 10:33:06 +0530

    Time: 3 ms (17.34%), Space: 9.9 MB (70.01%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int secondsBetweenTimes(string startTime, string endTime) {
-        return seconds(endTime) - seconds(startTime);
-    }
+        int he = stoi(endTime.substr(0, 2)), hs = stoi(startTime.substr(0, 2));
+        int me = stoi(endTime.substr(3, 2)), ms = stoi(startTime.substr(3, 2));
+        int se = stoi(endTime.substr(6, 2)), ss = stoi(startTime.substr(6, 2));
 
-private:
-    int seconds(string str) {
-        int cnt = 0;
+        int ans = 0;
+        ans += (he - hs - 1) * 60 * 60;
+        ans += (60 - ms - 1 + me) * 60;
+        ans += (se + 60 - ss);
 
-        cnt += stoi(str.substr(0, 2)) * 60 * 60;
-        cnt += stoi(str.substr(3, 2)) * 60;
-        cnt += stoi(str.substr(6, 2));
-
-        return cnt;
+        return ans;
     }
 };
